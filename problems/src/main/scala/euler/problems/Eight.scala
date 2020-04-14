e79ffa3132ab9b1ffabb40077a1c28c7b487f4f6
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-15 00:09:14 +0100

    wip on eight

---
+++ problems/src/main/scala/euler/problems/Eight.scala
@@ -0,0 +1,40 @@
+package euler.problems
+
+/**
+  * n adjacent digits with largest product, return product
+  *
+  * @param s - number is entered as a string
+  * @param n - number of adjcent digits we're looking for
+  */
+class Eight(s: String, n: Int) extends Problem {
+  def run() {
+    var i = 0
+    var l = s.length()
+    var div = 1
+    var mult = 1
+
+    // initialize largest to take the value of the first n digits of s
+    var sequence = s.substring(0, n)
+    var current = sequence.split("").map(c => c.toInt).reduce((x, y) => x*y)
+    var clargest = current
+
+    while (i + n < l) {
+      i+=1 // looking at substring that starts at i+1
+
+      val seq = s.substring(i, i + n)
+      println(s"Looking at sequence $seq")
+
+      div = s.charAt(i - 1).toString.toInt
+      mult = s.charAt(i + n - 1).toString.toInt
+
+      current = current * mult / div
+      if (current > clargest) {
+        clargest = current
+        sequence = seq
+      }
+    }
+
+    println(clargest)
+    println(sequence)
+  }
+}
