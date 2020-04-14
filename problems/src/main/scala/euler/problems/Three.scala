f05e679cbb748f06f6acf73840aa678bbc455a0f
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-14 17:38:43 +0100

    solved problem 3

--- problems/src/main/scala/euler/problems/Three.scala
+++ problems/src/main/scala/euler/problems/Three.scala
@@ -1,8 +1,39 @@
 package euler.problems
 
 /** Problem 3 asks to find the largest prime factor of 600851475143 */
-class Three extends Problem {
-  def run() {
+class Three(n: Long) extends Problem {
+
+  def prime(n: Long): Boolean = {
+    if (n == 1) return false
     
+    var j = 2
+    while (j*j < n) {
+      if (n % j == 0) return false
+      j += 1
+    }
+
+    return true
+  }
+
+  def run() {
+    var largest = 2L;
+
+    if (prime(n)) {
+      println(n)
+      return
+    }
+
+    var i = n;
+    while (i > 1) {
+      if (prime(largest) && (i % largest == 0)) {
+        do {
+          i /= largest
+        } while (i % largest == 0)
+      } else {
+        largest += 1
+      }
+    }
+
+    println(largest)
   }
 }
--- src/main/scala/euler/Main.scala
+++ src/main/scala/euler/Main.scala
@@ -4,7 +4,7 @@ import problems._
 
 object Main {
   def main(args: Array[String]) {
-    var p = new Two()
+    var p = new Three(600851475143L)
 
     p.run()
   }
