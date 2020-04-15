5bcdb3c9d1d5e1cfc061ec8271e45e9898f660bd
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-15 22:24:08 +0100

    starting nine

---
+++ problems/src/main/scala/euler/problems/Nine.scala
@@ -0,0 +1,11 @@
+package euler.problems
+
+/**
+  * Nine asks to find the product of a,b, and c, where a^2 + b^2 = c^2 and 
+  * a + b + c = 1000
+  */
+class Nine() extends Problem {
+  def run() {
+
+  }
+}
