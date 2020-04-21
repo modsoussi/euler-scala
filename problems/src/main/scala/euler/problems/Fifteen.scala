472af2d3786746601b4f0b20858ce530e6fe9518
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-21 19:26:28 +0100

    finished problem 15

--- problems/src/main/scala/euler/problems/Fifteen.scala
+++ problems/src/main/scala/euler/problems/Fifteen.scala
@@ -7,21 +7,42 @@ package euler.problems
   * @param n
   */
 class Fifteen(n: Int) extends Problem {
+  val _g = Array.ofDim[Long](n+1, n+1)
   var numPaths: Long = 0
 
-  def traverse(i: Int, j: Int) {
+  /**
+    * O(2^(n^2))
+    *
+    * @param i
+    * @param j
+    */
+  def slowTraverse(i: Int, j: Int) {
     if (i == n && j == n) {
-      numPaths += 1
+      numPaths += 1L
       return
     }
 
-    if (i < n) traverse(i+1, j)
-    if (j < n) traverse(i, j+1)
+    if (i < n) slowTraverse(i+1, j)
+    if (j < n) slowTraverse(i, j+1)
+  }
+
+  def traverse() {
+    _g(0)(0) = 0
+    for (i <- 1 to n) {
+      _g(0)(i) = 1
+      _g(i)(0) = 1
+    }
+
+    for (k <- 1 to n) {
+      for (l <- 1 to n) {
+        _g(k)(l) = _g(k)(l-1) + _g(k-1)(l)
+      }
+    }
   }
 
   def run() {
-    traverse(0, 0)
+    traverse()
 
-    println(numPaths)
+    println(_g(n)(n))
   }
 }
