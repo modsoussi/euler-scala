c04f77071f1386e7adf2635ff99f56ae14b919cf
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-24 00:52:46 +0100

    finished seventeen

--- problems/src/main/scala/euler/problems/Seventeen.scala
+++ problems/src/main/scala/euler/problems/Seventeen.scala
@@ -1,35 +1,22 @@
 package euler.problems
 
 class Seventeen extends Problem {
-  def getUnitNumberString(n: Int): String = {
-    n match {
-      case 1 => return "one"
-      case 2 => return "two"
-      case 3 => return "three"
-      case 4 => return "four"
-      case 5 => return "five"
-      case 6 => return "six"
-      case 7 => return "seven"
-      case 8 => return "eight"
-      case 9 => return "nine"
-    }
-  }
-
-  def getTensNumberString(n: Int): String = {
-    n match {
-      case 2 => return "twenty"
-      case 3 => return "thirty"
-      case 4 => return "fourty"
-      case 5 => return "fifty"
-      case 6 => return "sixty"
-      case 7 => return "seventy"
-      case 8 => return "eighty"
-      case 9 => return "ninety"
-    }
-  }
-
   def toNumberString(n: Int): String = {
     n match {
+      case x if 1 to 9 contains x => {
+        x match {
+          case 0 => return ""
+          case 1 => return "one"
+          case 2 => return "two"
+          case 3 => return "three"
+          case 4 => return "four"
+          case 5 => return "five"
+          case 6 => return "six"
+          case 7 => return "seven"
+          case 8 => return "eight"
+          case 9 => return "nine"
+        }
+      }
       case 10 => return "ten"
       case 11 => return "eleven"
       case 12 => return "twelve"
@@ -40,38 +27,52 @@ class Seventeen extends Problem {
       case 17 => return "seventeen"
       case 18 => return "eighteen"
       case 19 => return "nineteen"
-      case 1000 => return "one thousand"
-      case _ => {
-        var hundreds = n / 100
-        var tens = (n % 100) / 10
-        var units = n % 10
-        println(hundreds, tens, units)
-
-        var hundredsString = ""
-        if (hundreds > 0) {
-          hundredsString = s"${} hundred"
-        }
-
-        var result = getUnitNumberString(units)
-        if (tens > 0){
-          result = s"${getTensNumberString(tens)}-$result"
-        }
-
-        if (hundreds > 0) {
-          var temp = s"${getUnitNumberString(hundreds)} hundred"
-          if (result.length() > 0) {
-            result = s"and $result"
-          }
-
-          result = s"$temp $result"
-        }
-
-        return result
+      case x if 20 to 29 contains x => {
+        if (x == 20) return "twenty"
+        return s"twenty${toNumberString(x%10)}"
       }
+      case x if 30 to 39 contains x => {
+        if (x == 30) return "thirty"
+        return s"thirty${toNumberString(x%10)}"
+      }
+      case x if 40 to 49 contains x => {
+        if (x == 40) return "forty"
+        return s"forty${toNumberString(x%10)}"
+      }
+      case x if 50 to 59 contains x => {
+        if (x == 50) return "fifty"
+        return s"fifty${toNumberString(x%10)}"
+      }
+      case x if 60 to 69 contains x => {
+        if (x == 60) return "sixty"
+        return s"sixty${toNumberString(x%10)}"
+      }
+      case x if 70 to 79 contains x => {
+        if (x == 70) return "seventy"
+        return s"seventy${toNumberString(x%10)}"
+      }
+      case x if 80 to 89 contains x => {
+        if (x == 80) return "eighty"
+        return s"eighty${toNumberString(x%10)}"
+      }
+      case x if 90 to 99 contains x => {
+        if (x == 90) return "ninety"
+        return s"ninety${toNumberString(x%10)}"
+      }
+      case x if 100 to 999 contains x => {
+        if (x%100 == 0) return s"${toNumberString(x/100)}hundred"
+        return s"${toNumberString(x/100)}hundredand${toNumberString(x%100)}"
+      }
+      case 1000 => return "onethousand"
     }
   }
 
   def run() {
-    println(toNumberString(111))
+    var sum = 0
+    for (i <- 1 to 1000) {
+      sum += toNumberString(i).length()
+    }
+
+    println(sum)
   }
 }
