f38b799ae4fbfd1103859abcc8d6d8684da0ee4c
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-04-27 16:39:48 +0100

    started eighteen

--- problems/src/main/scala/euler/problems/Eighteen.scala
+++ problems/src/main/scala/euler/problems/Eighteen.scala
@@ -1,7 +1,27 @@
 package euler.problems
 
 class Eighteen(t: String) extends Problem {
+  val _split = t.split("\n")
+  val _size = _split.length
+  val _g = Array.ofDim[Long](_size, _size)
+
+  def _parse() {
+    for (i <- 0 until _size) {
+      var __split = _split(i).split(" ").map(e => e.toLong)
+      for (j <- 0 to i) {
+        _g(i)(j) = __split(j)
+      }
+    }
+  }
+
   def run() {
-    println(t)
+    _parse()
+
+    for (i <- 0 until _size) {
+      for (j <- 0 until _size) {
+        print(f"${_g(i)(j)}%2d ")
+      }
+      println
+    }
   }
 }
