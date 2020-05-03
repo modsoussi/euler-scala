62c468d2b90b6cf6b6368f54c24177344fc15093
Mohamed Soussi <mohameddsoussi@gmail.com>
2020-05-03 18:11:32 +0100

    finished 18

--- problems/src/main/scala/euler/problems/Eighteen.scala
+++ problems/src/main/scala/euler/problems/Eighteen.scala
@@ -17,16 +17,28 @@ class Eighteen(t: String) extends Problem {
   def _print() {
     for (i <- 0 until _size) {
       for (j <- 0 until _size) {
-        print(f"${_g(i)(j)}%2d ")
+        print(f"${_g(i)(j)}%4d ")
       }
       println
     }
+    println
   }
 
   def run() {
     _parse()
     _print()
 
-    
+    for (i <- 1 until _size) {
+      for (j <- 0 to i) {
+        _g(i)(j) = j match {
+          case 0 => _g(i)(j) + _g(i-1)(j)
+          case x if x == i => _g(i)(j) + _g(i-1)(j-1)
+          case _ => Math.max(_g(i)(j) + _g(i-1)(j-1), _g(i)(j) + _g(i-1)(j))
+        }
+      }
+    }
+
+    _print()
+    println(_g(_size-1).max)
   }
 }
